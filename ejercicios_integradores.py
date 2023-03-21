d460fa84b375495cfb5c2dacfcaba3a211190484
marianyudi <128326316+marianyudi@users.noreply.github.com>
2023-03-21 00:35:40 -0300

    Agregado cuenta joven

--- ejercicios_integradores.py
+++ ejercicios_integradores.py
@@ -73,4 +73,30 @@ class Cuenta:
 
 # Vamos a definir ahora una “Cuenta Joven”, para ello vamos a crear una nueva clase CuantaJoven que deriva de la clase creada en el punto 7. Cuando se crea esta nueva clase, además del titular y la cantidad se debe guardar una bonificación que estará expresada en tanto por ciento. Crear los siguientes métodos para la clase: • Un constructor. • Los setters y getters para el nuevo atributo. • En esta ocasión los titulares de este tipo de cuenta tienen que ser mayor de edad, por lo tanto hay que crear un método es_titular_valido() que devuelve verdadero si el titular es mayor de edad pero menor de 25 años y falso en caso contrario. • Además, la retirada de dinero sólo se podrá hacer si el titular es válido. • El método mostrar() debe devolver el mensaje de “Cuenta Joven” y la bonificación de la cuenta.
  
+class CuentaJoven(Cuenta):
+    def __init__(self, titular:Persona, cantidad=0.0, bonificacion=0.0):
+        super().__init__(titular, cantidad)
+        self._bonificacion = bonificacion
+    
+    def set_bonificacion(self, bonificacion):
+        self._bonificacion = bonificacion
+    
+    def get_bonificacion(self):
+        return self._bonificacion
+    
+    def es_titular_valido(self):
+        edad_maxima = 25
+        
+        edad = self.get_titular().get_edad()
+        if self.get_titular().es_mayor_de_edad() and edad < edad_maxima:
+            return True
+        return False
+            
+    def retirar(self, dinero):
+        if self.es_titular_valido():
+            super().retirar(dinero)
+    
+    def mostrar(self):
+        print("Cuenta Joven")
+        print("Bonificación de la cuenta:", self._bonificacion)
         
